e7035b9cfc77405edfb753a604abce462423f680
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2025-02-20 08:40:20 +0530

    Redux initialized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { RouterProvider } from "react-router";
 import router from "./router/Routes";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App: FC = () => {
     return (
-        <>
+        <Provider store={store}>
             <RouterProvider router={router} />
-        </>
+        </Provider>
     );
 };
 
