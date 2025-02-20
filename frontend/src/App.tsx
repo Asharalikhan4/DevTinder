3334f99af31a601b1b9ed79b9e5a28a571353ac3
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2025-02-20 08:46:49 +0530

    user slice added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { FC } from "react";
 import { RouterProvider } from "react-router";
 import router from "./router/Routes";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import appStore from "./redux/appStore";
 
 const App: FC = () => {
     return (
-        <Provider store={store}>
+        <Provider store={appStore}>
             <RouterProvider router={router} />
         </Provider>
     );
