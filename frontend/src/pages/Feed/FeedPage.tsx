2e3587c86abbcafc22ce782eecf834071c96904f
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2025-03-01 17:16:56 +0530

    Static feed page added with slice

--- frontend/src/pages/Feed/FeedPage.tsx
+++ frontend/src/pages/Feed/FeedPage.tsx
@@ -1,6 +1,32 @@
-import { FC } from "react";
+import { addFeed } from "@/redux/slices/feedSlice";
+import axios from "axios";
+import { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const FeedPage: FC = () => {
+
+    const dispatch = useDispatch();
+
+    const feed = useSelector((state: any) => state.feed);
+
+    const getFeed = async () => {
+        try {
+            const response = await axios.get(`${process.env.BASE_URL}/feed`, {
+                withCredentials: true
+            });
+            dispatch(addFeed(response.data));
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        if (!feed) {
+            getFeed();
+        };
+    }, []);
+
     return (
         <div>
             <h1 className="text-4xl">Home Page</h1>
